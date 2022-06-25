27b22fdde1aba5cf8284717ba876e2de60a9699b
UpgradeOfficial <odeyemiincrease@yahoo.com>
2022-06-25 17:02:58 +0100

    initial commit

---
+++ android/app/src/main/kotlin/ng/thinkalpha/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package ng.thinkalpha.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter 11 Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Here"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              final emailverified = user?.emailVerified ?? false;
+              if (emailverified) {
+                print("you are verified");
+              } else {
+                print("YOu are not verified");
+              }
+              return const Text("Done");
+            default:
+              return const Text("This is Loading");
+          }
+        },
+      ),
+    );
+  }
+}
